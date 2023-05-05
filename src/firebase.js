acf58e938e2209ce1fb3dc232593cb3115689f58
clspeter <mustpe@gmail.com>
2023-05-06 01:08:55 +0800

    eslint and cleanup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAfTw5AvtWiV3tDQt18xL9MTP4WUnjv9s",
+    authDomain: "nextflix-54929.firebaseapp.com",
+    projectId: "nextflix-54929",
+    storageBucket: "nextflix-54929.appspot.com",
+    messagingSenderId: "526139113186",
+    appId: "1:526139113186:web:914bf49c1be870556be8bb"
+};
