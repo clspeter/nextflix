f9167d82a85d0f8c4258ffd1c96599780670e5e0
clspeter <mustpe@gmail.com>
2023-05-10 21:17:07 +0800

    feat: rows

---
+++ src/Row.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Row.css'
+import axios from './api/axios'
+import PropTypes from 'prop-types'
+
+Row.propTypes = {
+    title: PropTypes.string.isRequired,
+    fetchUrl: PropTypes.string.isRequired,
+    isLargeRow: PropTypes.bool
+}
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+    const [movies, setMovies] = React.useState([])
+    const [poster, setPoster] = React.useState('')
+    const base_url = "https://image.tmdb.org/t/p/original/"
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl)
+            setMovies(request.data.results)
+            return request
+        }
+        fetchData()
+    }, [fetchUrl])
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            {movies.map(movie => (
+                <img
+                    key={movie.id}
+                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                    alt={movie.name}
+                ></img>
+            ))}
+
+        </div>
+    )
+}
+
+export default Row;
