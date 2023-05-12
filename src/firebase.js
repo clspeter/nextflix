27ab4ede688336468c98f957bf94c65239a219e0
clspeter <mustpe@gmail.com>
2023-05-13 01:36:22 +0800

    hide firebase secret

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCAfTw5AvtWiV3tDQt18xL9MTP4WUnjv9s",
-    authDomain: "nextflix-54929.firebaseapp.com",
-    projectId: "nextflix-54929",
-    storageBucket: "nextflix-54929.appspot.com",
-    messagingSenderId: "526139113186",
-    appId: "1:526139113186:web:914bf49c1be870556be8bb"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
