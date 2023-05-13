392ad8b76302ee056a1eaa6651f0a482259d53f5
clspeter <mustpe@gmail.com>
2023-05-13 20:47:52 +0800

    small fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from "react-router-dom";
 import './Nav.css'
 
 export default function Nav(isSignedIn = false) {
     const [show, handleShow] = React.useState(false);
+    const navigate = useNavigate();
 
     const transitionNavBar = () => {
         if (window.scrollY > 100) {
@@ -20,8 +22,9 @@ export default function Nav(isSignedIn = false) {
     return (
         <div className={`nav ${show && 'nav_black'}`}>
             <div className="nav_contents">
-                <img className="nav_logo" src="/nextflix-logo.png" alt="" />
-                <img className="nav_avatar" src="/nextflix-avatar.png" alt="" />
+                <img className="nav_logo" src="/nextflix-logo.png" alt=""
+                    onClick={() => navigate('/')} />
+                <img className="nav_avatar" src="/nextflix-avatar.png" alt="" onClick={() => navigate('/profile')} />
             </div>
         </div>
     )
